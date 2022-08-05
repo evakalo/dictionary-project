8f13d2cbaa1db5a043bb89a25e573cf0f9498724
evakalo <99054186+evakalo@users.noreply.github.com>
2022-08-05 21:29:57 +0200

    added picture and heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import book from "./book.png";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <header className="App-header">
+        {" "}
+        <h1>Dictionary</h1>
+        <img src={book} className="book-logo" alt="book" />
+      </header>
     </div>
   );
 }
